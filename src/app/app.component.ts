c32bf88df0e19b1aec470d6fd794bb46c57c3095
rocky3127 <rkdarlami@gmail.com>
2020-08-20 11:08:04 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
 import { Component } from '@angular/core';
-
+import { CommonService } from './common.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  allUsers: any;
+  isEdit = false;
+  userObj = {
+    name: '',
+    mobile: '',
+    email: '',
+    pasword: '',
+    id: ''
+  }
+  constructor( private commonService: CommonService) {}
   title = 'acrudjson';
+  ngOnInit() {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.getLatestUsers();
+    
+  }
+  addUser(user) {
+    console.log(user);
+    this.commonService.createUser(user).subscribe((response) => {
+      this.getLatestUsers();
+    })
+    
+  }
+  getLatestUsers(){
+    this.commonService.getAllUsers().subscribe((response) => {
+      this.allUsers = response;
+      console.log(response);
+      
+    })
+  }
+  deleteUser(user){
+    this.commonService.deleteUser(user).subscribe(() => {
+      this.getLatestUsers();
+    })
+  }
+  editUser(user) {
+    this.isEdit = true;
+    this.userObj = user;
+  }
+  updateUser() {
+    console.log(this.userObj);
+    
+    this.isEdit = !this.isEdit;
+    this.commonService.updateUser(this.userObj).subscribe(() => {
+      this.getLatestUsers();
+
+    })
+  }
 }
---
+++ src/app/common.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class CommonService {
+
+  constructor(private http: HttpClient ) { }
+  
+  createUser(user) {
+    return this.http.post('http://localhost:3000/users',user);
+  }
+  updateUser(user) {
+    return this.http.put('http://localhost:3000/users/' +user.id, user);
+  }
+  getAllUsers() {
+
+    return this.http.get('http://localhost:3000/users');
+  }
+  deleteUser(user) {
+    return this.http.delete('http://localhost:3000/users/' + user.id);
+  }
+}
